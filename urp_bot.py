0a488c03e7486cf2dc45f5da6b48de7ab7218d3d
Александр Жданов <canshok89@gmail.com>
2023-01-31 13:43:30 +0500

    Добавлено приветствие и кнопки

--- urp_bot.py
+++ urp_bot.py
@@ -4,31 +4,58 @@
 # Создан для УРП.
 
 import telebot
+from telebot import types
 
-API_TOKEN = ''
+API_TOKEN = '5958298312:AAHXm00wTnlftHdidKO5hdGpng7RE9_euck'
 bot = telebot.TeleBot(API_TOKEN)
 
 
 # Handle '/start' and '/help'
-@bot.message_handler(commands=['Старт'])
+@bot.message_handler(commands=['start'])
 def start(message):
-    mess = f'Привет, <b>{message.from_user.first_name} <u>{message.from_user.last_name}</u></b>'
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
+    website = types.KeyboardButton('Сайт_компании')
+    start = types.KeyboardButton('/Старт')
+    info = types.KeyboardButton('/info')
+    markup.add(website, start, info)
+    mess = f'Привет, <b>{message.from_user.first_name} {message.from_user.last_name}</b>!'
     bot.send_message(message.chat.id, mess, parse_mode='html')
 
+#сайт
+@bot.message_handler(commands=['website'])
+def website(message):
+    markup = types.InlineKeyboardMarkup()
+    markup.add(types.InlineKeyboardButton("Открыть сайт", url="http://gazpromneftenergysystems.ru"))
+    bot.send_message(message.chat.id, 'Откройте сайт', reply_markup=markup)
 
-# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
-@bot.message_handler(func=lambda message: True)
-def echo_message(message):
-    bot.reply_to(message, message.text)
+#информация для пользователя
+@bot.message_handler(commands=['info'])
+def send_welcome(message):
+    bot.reply_to(message, 'Я расскажу тебе о нефтесервисных активах!')
+
+#ответ на картинку
+@bot.message_handler(content_types=['photo'])
+def get_user_photo(message):
+    bot.send_message(message.chat.id, 'У меня нет глаз, я не понимаю что на этой картинке(')
 
 #кнопки помощи
-@bot.message_handler(commands=['help'])
-def website(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
-    website = types.KeyboardButton('Сайт_компании')
-    start = types.KeyboardButton('start')
-    can = types.KeyboardButton('info')
-    markup.add(website,start,can)
-    bot.send_message(message.chat.id, 'Используйте кнопки бота', reply_markup=markup)
+#@bot.message_handler(commands=['help'])
+#def website(message):
+    #markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+    #website = types.KeyboardButton('/Сайт_компании')
+    #start = types.KeyboardButton('/Старт')
+    #can = types.KeyboardButton('/info')
+    #markup.add(website, start, can)
+    #bot.send_message(message.chat.id, 'Используйте кнопки бота', reply_markup=markup)
+    #bot.send_message(message.chat.id, 'Main Menu', {reply_markup: {inline_keyboard: ["uno :+1:"],["uno \ud83d\udc4d", "due"],["uno", "due","tre"],["uno", "due","tre","quattro"]] } })
+
+#stop
+@bot.message_handler(commands=['stp'])
+def stop_command(message):
+    bot.send_message(message.chat.id, 'OK, stop...')
+    print("OK, stop...")
+    bot.stop_polling()
+
+
 
 bot.polling(none_stop=True)
