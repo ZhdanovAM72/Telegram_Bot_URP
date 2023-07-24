188a18d02e9694a0f9ccd4b37ef29242a0826be0
Александр Жданов <canshok89@gmail.com>
2023-07-24 10:25:37 +0500

    Added logger setting

---
+++ logger_setting/logger_bot.py
@@ -0,0 +1,26 @@
+import logging
+#from logging.handlers import RotatingFileHandler
+
+
+LOG_FILE = 'bot_log.txt'  # Имя файла логов
+
+
+def init_logger() -> logging.Logger:
+    """Определяем настройки логгера."""
+    logging.basicConfig(
+        format=('%(asctime)s - %(levelname)s - %(name)s - '
+                'строка: %(lineno)d - %(message)s'),
+        level=logging.INFO,
+        handlers=[
+            logging.StreamHandler(),
+            logging.handlers.RotatingFileHandler(
+                filename=LOG_FILE,
+                maxBytes=5_000_000,
+                backupCount=5
+            )
+        ],
+    )
+    return logging.getLogger(__name__)
+
+
+logger = init_logger()
