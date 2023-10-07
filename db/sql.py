a6c2300c3b3a8084f401d26ee31455f4e8f43820
Александр Жданов <canshok89@gmail.com>
2023-10-07 21:40:57 +0500

    Fix comments

--- db/sql.py
+++ db/sql.py
@@ -1,16 +1,8 @@
-import datetime as dt
 import sqlite3
 
 con = sqlite3.connect('users_v2.sqlite')
 cur = con.cursor()
 
-# cur.execute('''
-# CREATE TABLE IF NOT EXISTS bot_users(
-#     id INTEGER PRIMARY KEY,
-#     code TEXT,
-#     user_id INTEGER
-# );
-# ''')
 
 # cur.execute('''
 # CREATE TABLE IF NOT EXISTS bot_users(
@@ -27,19 +19,6 @@ cur = con.cursor()
 sql = "DELETE FROM bot_users WHERE id = 2"
 
 cur.execute(sql)
-
-# cur.execute('''
-# DELETE FROM bot_users(
-#     WHERE id = 1;
-# );
-# ''')
-
-# bot_users = [
-#     (1, 'admin-3javu0$%Q&k', 404025183, 'filmmaker89', 'Александр', 'Жданов', dt.datetime.now())
-# ]
-
-
-# cur.executemany('INSERT INTO bot_users VALUES(?, ?, ?, ?, ?, ?, ?);', bot_users)
 
 con.commit()
 
