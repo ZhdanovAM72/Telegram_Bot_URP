aaf731581fadd97baeefb3e24893dc356ab36e7b
Александр Жданов <canshok89@gmail.com>
2023-10-22 20:00:42 +0500

    Added comments

--- utils/password_generator.py
+++ utils/password_generator.py
@@ -3,7 +3,7 @@ from random import choice
 PASS_RANGE = 16
 EASY_PASS_RANGE = 4
 
-
+# Базовый генератор кодов (сложные значения)
 # def generate_code() -> str:
 #     """Генератор уникального кода доступа."""
 #     digits: str = '0123456789'
@@ -22,22 +22,23 @@ EASY_PASS_RANGE = 4
 
 
 def generate_code(company) -> str:
-    """Генератор уникального кода доступа."""
-    if company == 'es':
+    """Генератор уникального кода доступа по ДО."""
+    if company == 'es':  # Энергосистемы
         code = easy_code()
         return company + code
-    if company == 'st':
+    if company == 'st':  # Сервисные технологии
         code = easy_code()
         return company + code
-    if company == 'nr':
+    if company == 'nr':  # Нефтесервисные решения
         code = easy_code()
         return company + code
-    if company == 'its':
+    if company == 'its':  # Инженерно-технологический сервис
         code = easy_code()
         return company + code
     return False
 
 
+# Упрощенный генератор
 def easy_code() -> str:
     """Генератор уникального кода доступа."""
     digits: str = '0123456789'
