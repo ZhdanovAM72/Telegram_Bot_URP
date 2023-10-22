5846a2a2af14ae515967f46e0b82e5f98752a1d3
Александр Жданов <canshok89@gmail.com>
2023-10-22 23:42:03 +0500

    Added update code fiture

--- db/db_users.py
+++ db/db_users.py
@@ -86,3 +86,26 @@ def create_new_moderator(code: str, user_id: int) -> None:
         if con:
             con.close()
             logger.info('Закрыто соединение с БД: users_v2')
+
+
+def update_user_code(old_code: str, new_code: int) -> None:
+    """Обновляем код в БД."""
+    try:
+        con = sqlite3.connect('users_v2.sqlite')
+        cur = con.cursor()
+        sql_update_v1 = ("""
+            UPDATE bot_users
+            SET auth_code = ?
+            WHERE auth_code = ?
+        """)
+        data = (new_code, old_code)
+        cur.execute(sql_update_v1, data)
+        con.commit()
+        logger.info('Записан новый код в БД: '
+                    f'{new_code}')
+    except sqlite3.Error as error:
+        logger.error(f'SQL error: {error}')
+    finally:
+        if con:
+            con.close()
+            logger.info('Закрыто соединение с БД: users_v2')
