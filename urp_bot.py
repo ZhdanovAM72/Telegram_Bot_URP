151f88d4720c480bc4faf1ee5a13b3a8bea75194
Александр Жданов <canshok89@gmail.com>
2023-11-12 22:29:25 +0500

    Fix db search methods

--- urp_bot.py
+++ urp_bot.py
@@ -14,6 +14,7 @@ from db.permissions import (
     get_moderator_access,
     get_user_access,
 )
+from db.search import search_user_id_in_db, search_code_in_db
 from logger_setting.logger_bot import logger
 from utils.password_generator import generate_code
 from utils.excel import excel_export
@@ -391,37 +392,6 @@ def check_user_permissions(message: telebot.types.Message):
         start(message)
     else:
         bot.send_message(message.chat.id, 'Непредвиденная ошибка.')
-
-
-def search_user_id_in_db(chat_id):
-    """Проверяем наличие user_id в БД."""
-    with sqlite3.connect('users_v2.sqlite') as conn:
-        cursor = conn.cursor()
-        search_db_user = ('SELECT user_id, auth_code '
-                          'FROM bot_users WHERE user_id=?')
-        cursor.execute(
-            search_db_user,
-            (chat_id,)
-        )
-        search_user = cursor.fetchone()
-        print(search_user)
-        cursor.close()
-        return search_user
-
-
-def search_code_in_db(code):
-    """Проверяем наличие кода доступа в БД."""
-    with sqlite3.connect('users_v2.sqlite') as conn:
-        cursor = conn.cursor()
-        search_db = ('SELECT auth_code, user_id '
-                     'FROM bot_users WHERE auth_code=?')
-        cursor.execute(
-            search_db,
-            (code,)
-        )
-        search_code = cursor.fetchone()
-        cursor.close()
-        return search_code
 
 
 @bot.message_handler(commands=['code'])
