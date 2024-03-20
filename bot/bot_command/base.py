5b096e9a5a7fba220efc485cb4231c01487e50cf
Александр Жданов <canshok89@gmail.com>
2024-03-20 17:14:31 +0500

    Updated imports

--- bot/bot_command/base.py
+++ bot/bot_command/base.py
@@ -2,12 +2,18 @@ from bot.bot_command.start import StartBotCommand
 from bot.bot_command.stop import StopBotCommand
 from bot.bot_command.admin import AdminBotCommands
 from bot.bot_command.moderator import ModeratorBotCommands
+from bot.bot_command.register import RegisterUserCommand
+from bot.bot_command.create_code import CreateCodeCommands
+from bot.bot_command.bot_info import BotInfoCommands
 
 
 class BaseBotCommands(
     StartBotCommand,
     StopBotCommand,
     AdminBotCommands,
-    ModeratorBotCommands
+    ModeratorBotCommands,
+    RegisterUserCommand,
+    CreateCodeCommands,
+    BotInfoCommands,
 ):
     pass
