5bd3d26e4b6c8f245c5eb06a98a8c49195a9ca06
Александр Жданов <canshok89@gmail.com>
2024-03-28 12:51:18 +0500

    Updated methods

--- bot/utils/buttons.py
+++ bot/utils/buttons.py
@@ -6,7 +6,7 @@ class Buttons:
     # Функция для создания кнопок
     @staticmethod
     def create_keyboard_buttons(
-            buttons: list, row_with: int
+            buttons: list[str], row_with: int
     ) -> types.ReplyKeyboardMarkup:
         markup = types.ReplyKeyboardMarkup(
             resize_keyboard=True, row_width=row_with
--- bot/utils/documents.py
+++ bot/utils/documents.py
@@ -8,15 +8,16 @@ class Documents:
     # Функция для отправки документов
     def send_document_with_markup(
             chat_id: int,
-            document: str,
-            caption: str,
-            markup: types.ReplyKeyboardMarkup,
+            document: list[str],
+            caption: list[str] = None,
+            markup: types.ReplyKeyboardMarkup = None,
     ) -> types.Message:
-        with open(document, 'rb') as file:
-            bot.send_document(
-                chat_id,
-                file,
-                caption,
-                parse_mode="html",
-                reply_markup=markup,
-            )
+        for i in range(len(document)):
+            with open(document[i], 'rb') as file:
+                bot.send_document(
+                    chat_id,
+                    file,
+                    caption[i],
+                    parse_mode="html",
+                    reply_markup=markup,
+                )
