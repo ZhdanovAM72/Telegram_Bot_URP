d6f8f0937e02acb7764db421fadb1abd07a0e802
Александр Жданов <canshok89@gmail.com>
2024-03-29 22:02:44 +0500

    Added context processor base files

---
+++ bot/content_processor/text/base.py
@@ -0,0 +1,6 @@
+from bot.content_processor.text.main_menu import MainMenu
+from bot.content_processor.text.about_company import AboutCompany
+
+
+class BaseTextMenu(MainMenu, AboutCompany):
+    pass
