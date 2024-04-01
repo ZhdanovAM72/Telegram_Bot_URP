0d6d72f179c2ff567be8a485cfaab34c12333185
Александр Жданов <canshok89@gmail.com>
2024-04-01 16:52:44 +0500

    Updated AboutCompany methods

--- bot/content_processor/text/about_company.py
+++ bot/content_processor/text/about_company.py
@@ -2,11 +2,7 @@ from telebot import types
 from bot.utils.buttons import Buttons
 from bot.utils.documents import Documents
 from bot import bot
-from bot.constant import (
-    ES, ITS, NR, NNGGF, ST,
-    ABOUT_NTK,
-    NOT_REGISTERED,
-)
+from bot.constant import ES, ITS, NR, ST
 
 
 class AboutCompany:
@@ -56,12 +52,12 @@ class AboutCompany:
 
     def history_st(message: types.Message) -> types.Message:
         document = ['prod_data/о_компании/выбрать_ДО/СТ/история/about_us.pdf']
-        caption = ['История ООО "Газпромнефть Сервисные технологии"']
+        caption = [f'История {ST}']
         Documents.send_document_with_markup(message.chat.id, document, caption)
 
     def structure_st(message: types.Message) -> types.Message:
         document = ['prod_data/о_компании/выбрать_ДО/СТ/структура/structure.pdf']
-        caption = ['Структура ООО "Газпромнефть Сервисные технологии"']
+        caption = [f'Структура {ST}']
         Documents.send_document_with_markup(message.chat.id, document, caption)
 
     def do_nr(message: types.Message) -> types.Message:
@@ -78,7 +74,7 @@ class AboutCompany:
 
     def history_nr(message: types.Message) -> types.Message:
         document = ['prod_data/о_компании/выбрать_ДО/НР/История/about_us.pptx']
-        caption = ['История ООО "Нефтесервисные решения"']
+        caption = [f'История {NR}']
         Documents.send_document_with_markup(message.chat.id, document, caption)
 
     def do_its(message: types.Message) -> types.Message:
@@ -98,17 +94,17 @@ class AboutCompany:
 
     def contacts_its(message: types.Message) -> types.Message:
         document = ['prod_data/о_компании/выбрать_ДО/ННГГФ/Контакты/info.docx']
-        caption = ['Контакты ООО "Инженерно-технологический сервис"']
+        caption = [f'Контакты {ITS}']
         Documents.send_document_with_markup(message.chat.id, document, caption)
 
     def history_its(message: types.Message) -> types.Message:
         document = ['prod_data/о_компании/выбрать_ДО/ННГГФ/История/about_us.pdf']
-        caption = ['История ООО "Инженерно-технологический сервис"']
+        caption = [f'История {ITS}']
         Documents.send_document_with_markup(message.chat.id, document, caption)
 
     def structure_its(message: types.Message) -> types.Message:
         document = ['prod_data/о_компании/выбрать_ДО/ННГГФ/Структура/structure.pdf']
-        caption = ['Структура ООО "Инженерно-технологический сервис"']
+        caption = [f'Структура {ITS}']
         Documents.send_document_with_markup(message.chat.id, document, caption)
 
     def nmd_its(message: types.Message) -> types.Message:
@@ -123,4 +119,39 @@ class AboutCompany:
             f'Правила внутреннего трудового распорядка {ITS}',
             f'Положение о вахтовом методе работы {ITS}',
         ]
+        Documents.send_document_with_markup(message.chat.id, document, caption)
+
+    def do_es(message: types.Message) -> types.Message:
+        buttons = [
+            'История Энергосистем',
+            'Структура Энергосистем',
+            'Контакты Энергосистем',
+            '🔙 вернуться в раздел Выбрать ДО',
+        ]
+        markup = Buttons.create_keyboard_buttons(buttons, row_width=1)
+        bot.send_message(
+            message.from_user.id,
+            "⬇ Газпромнефть Энергосистемы",
+            reply_markup=markup
+        )
+
+    def history_es(message: types.Message) -> types.Message:
+        document = ['prod_data/о_компании/выбрать_ДО/ГПН_ЭС/история/о_нас.pptx']
+        caption = [f'История {ES}']
+        Documents.send_document_with_markup(message.chat.id, document, caption)
+
+    def structure_es(message: types.Message) -> types.Message:
+        document = ['prod_data/о_компании/выбрать_ДО/ГПН_ЭС/Структура/'
+                    'организационная_структура_ЭС.pdf']
+        caption = [f'Структура компании {ES}']
+        Documents.send_document_with_markup(message.chat.id, document, caption)
+
+    def contacts_es(message: types.Message) -> types.Message:
+        document = ['prod_data/о_компании/выбрать_ДО/ГПН_ЭС/контакты/contacs.pdf']
+        caption = [f'Контакты компании {ES}']
+        Documents.send_document_with_markup(message.chat.id, document, caption)
+
+    def corporate_values(message: types.Message) -> types.Message:
+        document = ['prod_data/о_компании/корпоративные_ценности/gpn_guide.pdf']
+        caption = ['Корпоративные ценности']
         Documents.send_document_with_markup(message.chat.id, document, caption)
