3023a87e92195e3c7c6dd194fdc0d4f3ed8adb39
Александр Жданов <canshok89@gmail.com>
2024-04-01 16:54:58 +0500

    Updated base content_processor text imports

--- bot/content_processor/text/base.py
+++ bot/content_processor/text/base.py
@@ -1,6 +1,10 @@
 from bot.content_processor.text.main_menu import MainMenu
 from bot.content_processor.text.about_company import AboutCompany
+from bot.content_processor.text.news_feed import NewsFeed
+from bot.content_processor.text.employees_services import EmployeesServices
+from bot.content_processor.text.adaptation import Adaptation
+from bot.content_processor.text.dms import DmsAndRvl
 
 
-class BaseTextMenu(MainMenu, AboutCompany):
+class BaseTextMenu(MainMenu, AboutCompany, NewsFeed, EmployeesServices, Adaptation, DmsAndRvl):
     pass
