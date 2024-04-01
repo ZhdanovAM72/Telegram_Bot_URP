bd50f7ece3ca98a4eb83b70eb8ebe6c386f2b0fb
Александр Жданов <canshok89@gmail.com>
2024-04-01 16:54:15 +0500

    Added create_inline_keyboard method

--- bot/utils/buttons.py
+++ bot/utils/buttons.py
@@ -6,7 +6,7 @@ class Buttons:
     # Функция для создания кнопок
     @staticmethod
     def create_keyboard_buttons(
-            buttons: list[str], row_width: int
+            buttons: list[str], row_width: int = 3
     ) -> types.ReplyKeyboardMarkup:
         markup = types.ReplyKeyboardMarkup(
             resize_keyboard=True, row_width=row_width
@@ -14,5 +14,18 @@ class Buttons:
         array = []
         for button_text in buttons:
             array.append(types.KeyboardButton(button_text))
+        markup.add(*array)
+        return markup
+
+    @staticmethod
+    def create_inline_keyboard(
+            buttons: list[str], row_width: int = 3
+    ) -> types.InlineKeyboardMarkup:
+        markup = types.InlineKeyboardMarkup(row_width=row_width)
+        array = []
+        for button_value in buttons:
+            array.append(
+                types.InlineKeyboardButton(text=button_value[0], url=button_value[1])
+            )
         markup.add(*array)
         return markup
