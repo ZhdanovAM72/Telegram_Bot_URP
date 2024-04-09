42e609ad23640c6240211188a8477cc2c59f5c96
Александр Жданов <canshok89@gmail.com>
2024-04-09 16:41:10 +0500

    Added CareerDevelopment text service

--- bot/content_processor/text/base.py
+++ bot/content_processor/text/base.py
@@ -4,7 +4,11 @@ from bot.content_processor.text.news_feed import NewsFeed
 from bot.content_processor.text.employees_services import EmployeesServices
 from bot.content_processor.text.adaptation import Adaptation
 from bot.content_processor.text.dms import DmsAndRvl
+from bot.content_processor.text.career_development import CareerDevelopment
 
 
-class BaseTextMenu(MainMenu, AboutCompany, NewsFeed, EmployeesServices, Adaptation, DmsAndRvl):
+class BaseTextMenu(
+    MainMenu, AboutCompany, NewsFeed, EmployeesServices,
+    Adaptation, DmsAndRvl, CareerDevelopment
+):
     pass
