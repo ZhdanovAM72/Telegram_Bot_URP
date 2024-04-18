e6647fdab0f085635444a547688a973bcbb24c2a
Александр Жданов <canshok89@gmail.com>
2024-04-18 20:21:54 +0500

    Updated ApplicationFormsBase class

--- bot/content_processor/text/application_forms/base_forms.py
+++ bot/content_processor/text/application_forms/base_forms.py
@@ -4,9 +4,15 @@ from bot.content_processor.text.application_forms.time_tracking import TimeTrack
 from bot.content_processor.text.application_forms.work_schedule import WorkSchedule
 from bot.content_processor.text.application_forms.day_off_working import DayOffWorking
 from bot.content_processor.text.application_forms.delay_in_transit import DelayTransit
+from bot.content_processor.text.application_forms.government_duties import GovernmentDuties
+from bot.content_processor.text.application_forms.birth_child import BirthChild
+from bot.content_processor.text.application_forms.termination_contract import TerminationEmploymentContract
+from bot.content_processor.text.application_forms.vacation_registration import VacationRegistration
+from bot.content_processor.text.application_forms.blood_donation import BloodDonation
 
 
 class ApplicationFormsBase(
-    ApplicationForms, AdvanceReport, WorkSchedule, TimeTracking, DayOffWorking, DelayTransit
+    ApplicationForms, AdvanceReport, WorkSchedule, TimeTracking, DayOffWorking, DelayTransit,
+    GovernmentDuties, BirthChild, TerminationEmploymentContract, VacationRegistration, BloodDonation,
 ):
     pass
