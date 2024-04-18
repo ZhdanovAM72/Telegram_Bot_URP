1f86dff4f2fb7b45e715580025339d0633a8c4f5
Александр Жданов <canshok89@gmail.com>
2024-04-18 13:43:22 +0500

    Updated TalentManagementCycle methods

--- bot/content_processor/text/talent_management_cycle.py
+++ bot/content_processor/text/talent_management_cycle.py
@@ -2,7 +2,6 @@ from telebot import types
 from bot.utils.buttons import Buttons
 from bot.utils.documents import Documents
 from bot import bot
-# from bot.constant import ES, ITS, NR, ST
 
 
 class TalentManagementCycle:
@@ -22,35 +21,6 @@ class TalentManagementCycle:
             text="⬇ Цикл управления талантами",
             reply_markup=markup,
         )
-
-    def education(message: types.Message) -> types.Message:
-        buttons = [
-            'Цикл планирования обучения',
-            'Каталог программ',
-            'Полезная литература',
-            'Планирование обучения',
-            '🔙 вернуться в раздел Цикл управления талантами',
-        ]
-        markup = Buttons.create_keyboard_buttons(buttons, row_width=2)
-        bot.send_message(
-            message.from_user.id,
-            text="⬇ Обучение",
-            reply_markup=markup,
-        )
-
-    def planning_education(message: types.Message) -> types.Message:
-        parrent_path = 'prod_data/Обучение/ГПН_ЭС/plan/'
-        documents = (
-            {
-                'file': open(f'{parrent_path}employee.pdf', 'rb'),
-                'caption': 'Планирование обучения - Сотрудник',
-            },
-            {
-                'file': open(f'{parrent_path}supervisor.pdf', 'rb'),
-                'caption': 'Планирование обучения - Руководитель',
-            },
-        )
-        Documents.send_media_group_without_markup(message.chat.id, documents)
 
     def regular_assessment(message: types.Message) -> types.Message:
         parrent_path = 'prod_data/Цикл_управления_талантами/Регулярная_оценка/'
