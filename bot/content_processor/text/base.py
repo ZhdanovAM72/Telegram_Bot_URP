11511f2b21ff9a5a051b004db562a8f142c9834a
Александр Жданов <canshok89@gmail.com>
2024-04-18 13:44:00 +0500

    Updated BaseTextMenu class

--- bot/content_processor/text/base.py
+++ bot/content_processor/text/base.py
@@ -6,10 +6,15 @@ from bot.content_processor.text.adaptation import Adaptation
 from bot.content_processor.text.dms import DmsAndRvl
 from bot.content_processor.text.career_development import CareerDevelopment
 from bot.content_processor.text.talent_management_cycle import TalentManagementCycle
+from bot.content_processor.text.internship import Internship
+from bot.content_processor.text.education import Education
+from bot.content_processor.text.youth_policy import YouthPolicy
+from bot.content_processor.text.application_forms.base_forms import ApplicationFormsBase
 
 
 class BaseTextMenu(
-    MainMenu, AboutCompany, NewsFeed, EmployeesServices,
-    Adaptation, DmsAndRvl, CareerDevelopment, TalentManagementCycle
+    MainMenu, AboutCompany, NewsFeed, EmployeesServices, Education, YouthPolicy,
+    Adaptation, DmsAndRvl, CareerDevelopment, TalentManagementCycle, Internship,
+    ApplicationFormsBase,
 ):
     pass
