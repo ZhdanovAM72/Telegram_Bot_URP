bc4dc56f0741dd8dc3621075ca87ef7ce2933977
Александр Жданов <canshok89@gmail.com>
2024-04-20 20:29:15 +0500

    Updated get_user_stiсker method

--- bot/content_processor/sticker.py
+++ bot/content_processor/sticker.py
@@ -4,22 +4,15 @@ from bot import bot
 from bot.logger_setting.logger_bot import log_sticker
 from bot.utils.check_permission import CheckUserPermission
 
+TEXT = "У меня нет глаз, я не вижу этот стикер. Давайте продолжим работать в меню."
+
 
 class StiсkerProcessor:
 
-    @classmethod
-    def get_user_stiсker(cls, message: types.Message) -> None:
+    @staticmethod
+    def get_user_stiсker(message: types.Message) -> None | types.Message:
         """Ловим отправленные пользователем стикеры."""
         if not CheckUserPermission.check_user(message):
             return log_sticker(message)
-
-        bot.send_message(
-            message.chat.id,
-            text=(
-                '''
-                У меня нет глаз, я не вижу этот стикер.
-                Давайте продолжим работать в меню.
-                '''
-            ),
-        )
-        return log_sticker(message)
+        log_sticker(message)
+        return bot.send_message(message.chat.id, text=TEXT)
