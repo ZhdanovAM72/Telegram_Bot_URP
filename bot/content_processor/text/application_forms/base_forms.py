e92be92afb83c965c9801d3801fe6f29441f470a
Александр Жданов <canshok89@gmail.com>
2024-04-29 18:37:28 +0500

    Updated base class

--- bot/content_processor/text/application_forms/base_forms.py
+++ bot/content_processor/text/application_forms/base_forms.py
@@ -13,11 +13,14 @@ from bot.content_processor.text.application_forms.transfer_vacation import Trans
 from bot.content_processor.text.application_forms.vacation_without_pay import VacationWithoutPay
 from bot.content_processor.text.application_forms.cancellation_vacation import CancellationRecallVacation
 from bot.content_processor.text.application_forms.other_types_vacation import OtherTypesVacation
+from bot.content_processor.text.application_forms.change_employment_contract import ChangeEmploymentContract
+from bot.content_processor.text.application_forms.working_hours import WorkingHours
 
 
 class ApplicationFormsBase(
     ApplicationForms, AdvanceReport, WorkSchedule, TimeTracking, DayOffWorking, DelayTransit,
     GovernmentDuties, BirthChild, TerminationEmploymentContract, VacationRegistration, BloodDonation,
     TransferExtensionVacation, VacationWithoutPay, CancellationRecallVacation, OtherTypesVacation,
+    ChangeEmploymentContract, WorkingHours
 ):
     pass
