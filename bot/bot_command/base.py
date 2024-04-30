d2324070ef41032856e92e3ef6a3c615a1d8c6e0
Александр Жданов <canshok89@gmail.com>
2024-04-30 10:53:31 +0500

    Added SendingMessagesBotCommands methods

--- bot/bot_command/base.py
+++ bot/bot_command/base.py
@@ -5,6 +5,7 @@ from bot.bot_command.moderator import ModeratorBotCommands
 from bot.bot_command.register import RegisterUserCommand
 from bot.bot_command.create_code import CreateCodeCommands
 from bot.bot_command.bot_info import BotInfoCommands
+from bot.bot_command.sending_messages import SendingMessagesBotCommands
 
 
 class BaseBotCommands(
@@ -15,5 +16,6 @@ class BaseBotCommands(
     RegisterUserCommand,
     CreateCodeCommands,
     BotInfoCommands,
+    SendingMessagesBotCommands,
 ):
     pass
