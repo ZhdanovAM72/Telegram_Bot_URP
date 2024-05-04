8a4ab50eee5be80b88992bb26c0532026a106f9e
Александр Жданов <canshok89@gmail.com>
2024-05-04 23:16:51 +0500

    Updated start method

--- bot/bot_command/start.py
+++ bot/bot_command/start.py
@@ -2,30 +2,34 @@ import telebot
 from telebot import types
 
 from bot import bot
-from bot.logger_setting.logger_bot import log_user_command
+from bot.logger_setting.logger_bot import logger, log_user_command_updated
 from bot.utils.check_permission import CheckUserPermission
+from bot.utils.buttons import Buttons
+
+START_MESSAGE = (
+    'Здравствуйте, <b>{0}</b>!\n'
+    'Я расскажу Вам о нефтесервисных активах! '
+    'выберите интересующую Вас тему в меню.'
+)
 
 
 class StartBotCommand:
 
     @staticmethod
-    def start(message: telebot.types.Message):
+    def start(message: telebot.types.Message) -> types.Message | None:
         """Приветствуем пользователя и включаем меню бота."""
         if not CheckUserPermission.check_user(message):
+            logger.warning(log_user_command_updated(message))
             return None
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        button_1 = types.KeyboardButton('Информация о боте')
-        button_2 = types.KeyboardButton('Главное меню')
-        markup.add(button_1, button_2)
 
-        if (message.from_user.first_name is not None and
-           message.from_user.last_name is not None):
+        if (message.from_user.first_name is not None
+           and message.from_user.last_name is not None):
             user_info = (
                 f'{message.from_user.first_name} {message.from_user.last_name}'
             )
 
-        elif (message.from_user.first_name is not None and
-              message.from_user.last_name is None):
+        elif (message.from_user.first_name is not None
+              and message.from_user.last_name is None):
             user_info = message.from_user.first_name
 
         elif message.from_user.username is not None:
@@ -34,12 +38,16 @@ class StartBotCommand:
         else:
             user_info = 'сотрудник'
 
-        start_message = (f'Здравствуйте, <b>{user_info}</b>!\n'
-                         'Я расскажу Вам о нефтесервисных активах! '
-                         'выберите интересующую Вас тему в меню.')
-        bot.send_message(
+        buttons = (
+            'Информация о боте',
+            'Главное меню',
+        )
+        markup = Buttons.create_keyboard_buttons(buttons)
+
+        logger.info(log_user_command_updated(message))
+        return bot.send_message(
             message.chat.id,
-            start_message, parse_mode='html',
+            START_MESSAGE.format(user_info),
+            parse_mode='html',
             reply_markup=markup,
         )
-        return log_user_command(message)
