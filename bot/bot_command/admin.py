126f0eeedf56eb04b935fd59ec993bf23a807b35
Александр Жданов <canshok89@gmail.com>
2024-05-04 23:17:19 +0500

    Updated admin commands methods

--- bot/bot_command/admin.py
+++ bot/bot_command/admin.py
@@ -4,7 +4,7 @@ from bot import bot
 from bot.constants import ADMIN_COMMANDS
 from bot.db import BaseBotSQLMethods
 from bot.utils.code_generator import CodeGenerator
-from bot.logger_setting.logger_bot import log_user_command
+from bot.logger_setting.logger_bot import log_user_command, logger, log_user_command_updated
 from bot.utils.check_permission import CheckUserPermission
 
 
@@ -15,11 +15,11 @@ class AdminBotCommands:
         """"Направляем список доступных команд администратору."""
         bot.send_message(message.chat.id, 'Проверяем права.')
         if not CheckUserPermission.check_admin(message):
-            log_user_command(message)
+            logger.warning(log_user_command_updated(message))
             return None
-        bot.send_message(message.chat.id, 'Привет Admin!')
-        bot.send_message(message.chat.id, text=ADMIN_COMMANDS)
-        return log_user_command(message)
+        logger.info(log_user_command_updated(message))
+        return (bot.send_message(message.chat.id, 'Привет Admin!'),
+                bot.send_message(message.chat.id, text=ADMIN_COMMANDS))
 
     @classmethod
     def update_code(cls, message: types.Message) -> None:
