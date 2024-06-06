42607e75fa52c0dd5c8e735c56b1872aa2063f0d
Александр Жданов <canshok89@gmail.com>
2024-06-06 18:19:12 +0500

    Added postgres settings

---
+++ bot/db/postgers_settings.py
@@ -0,0 +1,30 @@
+import os
+from pydantic import SecretStr
+from pydantic_settings import BaseSettings
+from sqlalchemy import create_engine, URL
+
+
+class PostgresSettings(BaseSettings):
+    """Конфигурация PostgresQL."""
+
+    POSTGRES_HOST: str = os.getenv('POSTGRES_HOST')
+    POSTGRES_PORT: int = os.getenv('POSTGRES_PORT')
+    POSTGRES_USER: str = os.getenv('POSTGRES_USER')
+    POSTGRES_PASSWORD: SecretStr = SecretStr(os.getenv('POSTGRES_PASSWORD'))
+    POSTGRES_DB: str = os.getenv('POSTGRES_DB')
+
+    @property
+    def postgres_connection_url(self) -> URL:
+        return URL.create(
+            drivername="postgresql+psycopg2",
+            username=self.POSTGRES_USER,
+            password=self.POSTGRES_PASSWORD.get_secret_value(),
+            host=self.POSTGRES_HOST,
+            port=self.POSTGRES_PORT,
+            database=self.POSTGRES_DB,
+        )
+
+
+settings = PostgresSettings()
+
+engine = create_engine(settings.postgres_connection_url)
