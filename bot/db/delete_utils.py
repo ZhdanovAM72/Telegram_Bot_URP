50f69495533892c0b2a79f314161dec4cb757648
Александр Жданов <canshok89@gmail.com>
2024-06-23 14:15:48 +0500

    Updated DeleteMethods

--- bot/db/delete_utils.py
+++ bot/db/delete_utils.py
@@ -1,50 +1,64 @@
-import sqlite3
+import datetime as dt
+
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+from telebot.types import Message
 
 from bot.logger_setting.logger_bot import logger
+from bot.db.database import Database, PostgresSettings
+from bot.db.entities.users import User
 
 
 class DeleteMethods:
+    settings = PostgresSettings()
+    db = Database(settings)
 
     @classmethod
-    def delete_by_chat_id(cls, chat_id: int) -> None:
-        """Удаляем данные пользователя по user_id."""
+    def delete_email(cls, email: str, session: Session = None) -> bool:
         try:
-            con = sqlite3.connect('users_v2.sqlite')
-            cur = con.cursor()
-            sql_update_v1 = ("""
-                DELETE FROM bot_users
-                WHERE user_id = ?;
-            """)
-            cur.execute(sql_update_v1, (chat_id,))
-            con.commit()
-            cur.close()
-            logger.info('Команда удаления пользователя в БД, id: '
-                        f'{chat_id}')
-        except sqlite3.Error as error:
-            logger.error(f'SQL error: {error}')
-        finally:
-            if con:
-                con.close()
-                logger.info('Закрыто соединение с БД: users_v2')
+            with cls.db.get_session(session) as session:
+                user_query = session.execute(select(User).where(User.email == email))
+                user = user_query.scalars().first()
+                if user.email and not (user.is_admin or user.is_moderator):
+                    logger.info(f"Удаление успешно! Пользователь: {email}")
+                    session.delete(user)
+                    session.commit()
+                    return True
+                logger.info(f"Удаление неуспешно! Пользователь: {email}")
+                return False
+        except Exception as e:
+            logger.error(f"Ошибка при удалении пользователя с данными: {email} - {e}")
 
     @classmethod
-    def delete_by_code(cls, auth_code: str) -> None:
-        """Удаляем все данные привязанные с auth_code."""
+    def delete_moderator_by_email(cls, email: str, message: Message, session: Session = None) -> bool:
         try:
-            con = sqlite3.connect('users_v2.sqlite')
-            cur = con.cursor()
-            sql_update_v1 = ("""
-                DELETE FROM bot_users
-                WHERE auth_code = ?;
-            """)
-            cur.execute(sql_update_v1, (auth_code,))
-            con.commit()
-            cur.close()
-            logger.info('Команда удаления дынных из БД по коду: '
-                        f'{auth_code}')
-        except sqlite3.Error as error:
-            logger.error(f'SQL error: {error}')
-        finally:
-            if con:
-                con.close()
-                logger.info('Закрыто соединение с БД: users_v2')
+            with cls.db.get_session(session) as session:
+                user_query = session.execute(select(User).where(User.email == email))
+                user = user_query.scalars().first()
+                if user.is_moderator:
+                    logger.info(f"Удаление прав успешно! Пользователь: {email}")
+                    user.is_moderator = False
+                    user.updated_at = dt.datetime.fromtimestamp(message.date)
+                    session.commit()
+                    return True
+                logger.info(f"Удаление прав неуспешно! Пользователь: {email}")
+                return False
+        except Exception as e:
+            logger.error(f"Ошибка при удалении пользователя с данными: {email} - {e}")
+
+    @classmethod
+    def delete_admin_by_email(cls, email: str, message: Message, session: Session = None) -> bool:
+        try:
+            with cls.db.get_session(session) as session:
+                user_query = session.execute(select(User).where(User.email == email))
+                user = user_query.scalars().first()
+                if user.is_admin:
+                    logger.info(f"Удаление прав успешно! Пользователь: {email}")
+                    user.is_admin = False
+                    user.updated_at = dt.datetime.fromtimestamp(message.date)
+                    session.commit()
+                    return True
+                logger.info(f"Удаление прав неуспешно! Пользователь: {email}")
+                return False
+        except Exception as e:
+            logger.error(f"Ошибка при удалении пользователя с данными: {email} - {e}")
