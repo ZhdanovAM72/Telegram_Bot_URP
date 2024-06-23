70d77fcbd2844da837832ec2e6d8d527602547d7
Александр Жданов <canshok89@gmail.com>
2024-06-23 14:17:04 +0500

    Updated ExcelExport method

--- bot/utils/excel_export.py
+++ bot/utils/excel_export.py
@@ -1,24 +1,33 @@
-import sqlite3
-
+from psycopg2 import Error
 import pandas as pd
+from sqlalchemy import create_engine, null, not_
+from sqlalchemy.orm import Session
 
 from bot.logger_setting.logger_bot import logger
+from bot.db.database import Database, PostgresSettings
+from bot.db.entities.users import User
 
 
 class ExcelExport:
     """Класс для работы с Excel."""
+    settings = PostgresSettings()
+    db = Database(settings)
 
     @classmethod
-    def excel_export(cls):
-        """Выгрузка данных БД в excel."""
+    def excel_export_new(cls, session: Session = None):
         try:
-            con = sqlite3.connect('users_v2.sqlite')
-            df = pd.read_sql('SELECT * FROM bot_users', con)
-            df.to_excel('result.xlsx', index=False)
-            logger.info('Выгрузка БД в excel.')
-        except sqlite3.Error as error:
-            logger.error(f'SQL error: {error}')
-        finally:
-            if con:
-                con.close()
-                logger.info('Закрыто соединение с БД: users_v2')
+            with cls.db.get_session(session) as session:
+                users_count = session.query(User).where(not_(User.telegram_id == null())).count()
+                accounts_count = session.query(User).where(not_(User.full_name == null())).count()
+                engine = create_engine(cls.settings.postgres_connection_url)
+                df = pd.read_sql_table('users', engine)
+                if df.empty:
+                    logger.info("No data to export.")
+                    return None
+                logger.info('Выгрузка БД в excel.')
+                df.to_excel('result.xlsx', index=False)
+                return accounts_count, users_count
+        except Error as e:
+            logger.error(f"Ошибка при подключении к БД: {e}")
+        except Exception as e:
+            logger.error(f"Ошибка при выгрузке данных в Excel: {e}")
