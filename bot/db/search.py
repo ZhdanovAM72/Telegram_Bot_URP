1142e3b372653b016915b7baf756d92187cb25a3
Александр Жданов <canshok89@gmail.com>
2024-06-23 14:16:19 +0500

    Updated SearchMethods

--- bot/db/search.py
+++ bot/db/search.py
@@ -1,8 +1,5 @@
-import sqlite3
-
-from sqlalchemy import select
+from sqlalchemy import select, not_
 from sqlalchemy.orm import Session
-# from telebot.types import Message
 
 from bot.logger_setting.logger_bot import logger
 from bot.db.database import Database, PostgresSettings
@@ -56,6 +53,32 @@ class SearchMethods:
             logger.error(f"Ошибка при регистрации на занятый id №: {telegram_id} - {e}")
 
     @classmethod
+    def search_telegram_id_user(cls, telegram_id: int, session: Session = None):
+        try:
+            with cls.db.get_session(session) as session:
+                user_query = session.execute(select(User).where(User.telegram_id == telegram_id))
+                user = user_query.scalars().first()
+                if not user:
+                    return False
+                logger.info("FIND ID NUMBER IN DB")
+                return user.telegram_id
+        except Exception as e:
+            logger.error(f"Ошибка при регистрации на занятый id №: {telegram_id} - {e}")
+
+    @classmethod
+    def search_email_user(cls, email: str, session: Session = None):
+        try:
+            with cls.db.get_session(session) as session:
+                user_query = session.execute(select(User).where(User.email == email))
+                user = user_query.scalars().first()
+                if not user:
+                    return False
+                logger.info("FIND EMAIL IN DB")
+                return user.telegram_id
+        except Exception as e:
+            logger.error(f"Ошибка поиска email: {email} - {e}")
+
+    @classmethod
     def search_full_name_in_db(cls, telegram_id: int, session: Session = None):
         try:
             with cls.db.get_session(session) as session:
@@ -70,45 +93,29 @@ class SearchMethods:
             logger.error(f"Ошибка при регистрации на занятый id №: {telegram_id} - {e}")
 
     @classmethod
-    def search_user_id_in_db(cls, chat_id):
-        """Проверяем наличие user_id в БД."""
-        with sqlite3.connect('users_v2.sqlite') as conn:
-            cursor = conn.cursor()
-            search_db_user = ('SELECT user_id, auth_code '
-                              'FROM bot_users WHERE user_id=?')
-            cursor.execute(
-                search_db_user,
-                (chat_id,)
-            )
-            search_user = cursor.fetchone()
-            cursor.close()
-            return search_user
+    def search_full_name_if_email(cls, email: str, session: Session = None):
+        try:
+            with cls.db.get_session(session) as session:
+                user_query = session.execute(select(User).where(User.email == email))
+                user = user_query.scalars().first()
+                if not user:
+                    session.close()
+                    return False
+                logger.info("FIND full_name NUMBER IN DB")
+                return user.full_name
+        except Exception as e:
+            logger.error(f"Ошибка поиска данных: {email} - {e}")
 
     @classmethod
-    def search_code_in_db(cls, code):
-        """Проверяем наличие кода доступа в БД."""
-        with sqlite3.connect('users_v2.sqlite') as conn:
-            cursor = conn.cursor()
-            search_db = ('SELECT auth_code, user_id '
-                         'FROM bot_users WHERE auth_code=?')
-            cursor.execute(
-                search_db,
-                (code,)
-            )
-            search_code = cursor.fetchone()
-            cursor.close()
-            return search_code
-
-    @classmethod
-    def search_all_user_id(cls):
-        """Поиск всех пользователей в базе данных."""
-        with sqlite3.connect('users_v2.sqlite') as conn:
-            cursor = conn.cursor()
-            cursor.execute(
-                """
-                SELECT user_id FROM bot_users WHERE user_id IS NOT NULL
-                """
-            )
-            search_all_ids = cursor.fetchall()
-            cursor.close()
-            return search_all_ids
+    def search_all_users(cls, session: Session = None):
+        try:
+            with cls.db.get_session(session) as session:
+                user = session.query(User).filter(not_(User.telegram_id.is_(None))).all()
+                logger.info(user)
+                if not user:
+                    session.close()
+                    return False
+                logger.info("FIND full_name NUMBER IN DB")
+                return user
+        except Exception as e:
+            logger.error(f"Ошибка поиска данных: {e}")
