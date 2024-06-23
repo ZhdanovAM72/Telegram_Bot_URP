49fd5fcc7f0605675baf3916529a1c1ebff87953
Александр Жданов <canshok89@gmail.com>
2024-06-23 14:13:25 +0500

    Updated export methods

--- bot/bot_command/bot_info.py
+++ bot/bot_command/bot_info.py
@@ -1,3 +1,4 @@
+import os
 import datetime as dt
 
 from telebot import types
@@ -19,18 +20,31 @@ class BotInfoCommands:
             return None
 
         bot.send_message(message.chat.id, 'Попытка экспорта данных.')
-        ExcelExport.excel_export()
+        accounts_count, users_count = ExcelExport.excel_export_new()
         date_info = dt.datetime.utcfromtimestamp(message.date)
 
-        documents = (
+        excel_document = (
             "result.xlsx",
-            "bot_log.txt",
-            "users_v2.sqlite",
         )
-        captions = (
-            f'Выгрузка БД на {date_info.date()}',
-            f'Логи на {date_info.date()}',
-            f'Файл БД {date_info.date()}',
+        excel_caption = (
+            f'Выгрузка дынных из БД на {date_info.date()}, \n'
+            f'Учетных записей всего: {accounts_count if accounts_count else 0} шт.\n'
+            f'Зарегистрировано: {users_count if users_count else 0} сотрудника(ов).',
         )
         logger.info(log_user_command_updated(message))
-        return Documents.send_document_with_markup(message.chat.id, documents, captions)
+        return Documents.send_document_with_markup(message.chat.id, excel_document, excel_caption)
+
+    @classmethod
+    def export_logs(cls, message: types.Message) -> types.Message | None:
+        """Экспортируем логи."""
+        if not CheckUserPermission.check_admin(message):
+            logger.warning(log_user_command_updated(message))
+            return None
+
+        bot.send_message(message.chat.id, 'Попытка экспорта данных.')
+        date_info = dt.datetime.utcfromtimestamp(message.date)
+
+        log_documents = [file for file in os.listdir('.') if file.startswith("bot_log.txt")]
+        log_caption = [f'Логи на {date_info.date()}' for _ in log_documents]
+        logger.info(log_user_command_updated(message))
+        return Documents.send_document_with_markup(message.chat.id, log_documents, log_caption)
