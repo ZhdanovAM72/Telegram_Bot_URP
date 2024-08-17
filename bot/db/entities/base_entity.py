41fcdd56daaa92b025a99600091284fcaddfbcd3
Александр Жданов <canshok89@gmail.com>
2024-08-17 12:55:57 +0500

    Fixed id type

--- bot/db/entities/base_entity.py
+++ bot/db/entities/base_entity.py
@@ -12,6 +12,6 @@ class Entity(Base):
 
     __abstract__ = True
 
-    id: Mapped[str] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True)
+    id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True)
     created_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
     updated_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now(), server_onupdate=func.now())
